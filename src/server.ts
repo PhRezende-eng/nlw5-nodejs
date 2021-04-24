27d751d74314cdf6053d31b9319cc24bc70f46a8
PhRezende-eng <ph.hr.001@gmail.com>
2021-04-24 01:49:29 -0300

    #Aula4 for a while, i just done a half of fourth video

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,4 @@
-import express, { request, response } from "express";
+import {http} from "./http";
+import "./websocket/client";
 
-import "./database";
-import { routes } from "./routes";
-
-
-const app = express();
-app.use(express.json());
-/**
- * GET = Buascar
- * POST = Criar / Cadastrar
- * PUT = Alterar
- * DELETE = Deletar
- * PATCH = Alterar algo já cadastrado(algo específico)
- */
-
-app.get("/", (request, response) => {
-    return response.json(
-        {
-            "message": "olá mundo!",
-        }
-    );
-});
-
-app.post("/users/user", (request, response) => {
-    return response.json({
-        message : "Usuário salvo!"
-    });
-})
-
-app.use(routes);
-
-app.listen(3333, ()=> console.log("tá rodando na porta  3333"));
+http.listen(3333, ()=> console.log("tá rodando na porta  3333"));
